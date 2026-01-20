a7ce11eb0a08371f229a64f22efec4134b45e0cd
Marvin Kioko <kiokomarvin7@gmail.com>
2026-01-20 21:44:03 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -44,8 +44,31 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            for attr in data:
+                setattr(plant, attr, data[attr])
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
